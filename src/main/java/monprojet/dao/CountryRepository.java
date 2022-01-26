1aae5a75d7f752d8a92d5df2986b2f11dbf76ac7
Rémi Bastide <Remi.Bastide@irit.fr>
2022-01-26 19:12:53 +0100

    Git authentication

--- src/main/java/monprojet/dao/CountryRepository.java
+++ src/main/java/monprojet/dao/CountryRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import monprojet.entity.Country;
 
 // This will be AUTO IMPLEMENTED by Spring 
+//
 
 public interface CountryRepository extends JpaRepository<Country, Integer> {
 
